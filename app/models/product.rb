935fe4ddf8248b9ef0e7f909984cc665709463af
Ashley Mason <amason@spoonflower.com>
2015-11-02 17:03:45 -0500

    add image to products on product index.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,10 @@
 class Product < ActiveRecord::Base
+  has_many :images
+  belongs_to :user
+
+  validates :name, presence: true
+  validates :yardage, presence: true
+  validates :fabric_type, presence: true
+  validates :shipping, presence: true
+  validates :price, presence: true
 end
