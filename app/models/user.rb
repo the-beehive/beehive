10970e6962431e42789fa6b67214217c3ad08bd2
joshneipp <joshneipp@gmail.com>
2015-11-03 17:32:47 -0500

    Alter validations on users.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,8 @@ class User < ActiveRecord::Base
   has_many :orders
 
   validates :name, presence: true
-  validates :username, presence: true
-  validates :email, presence: true
+  # validates :username, presence: true
+  # validates :email, presence: true
 
   def self.sign_in_from_omniauth(auth)
    find_by(provider: auth['provider'], uid: auth['uid']) || create_user_from_omniauth(auth)
