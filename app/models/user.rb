a5666825669d84a6188e79f27362fe149eeb012b
joshneipp <joshneipp@gmail.com>
2015-11-02 23:17:07 -0500

    Configure omniauth facebook and twitter for heroku.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ActiveRecord::Base
+
+   def self.sign_in_from_omniauth(auth)
+     find_by(provider: auth['provider'], uid: auth['uid']) || create_user_from_omniauth(auth)
+   end
+
+   def self.create_user_from_omniauth(auth)
+    create! do |user|
+      user.provider = auth["provider"]
+      user.uid = auth["uid"]
+      user.name = auth["info"]["name"]
+    end
+   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,12 @@ Rails.application.routes.draw do
   resources :users
   resources :images
   resources :products
+
+  get 'auth/facebook/callback', to: "sessions#create"
+  get 'auth/:provider/callback', to: 'sessions#create'
+
+  delete 'sign_out', to: "sessions#destroy", as: 'sign_out'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
