cc61c0f41028b64a2f0e92dc1b8a90a6c57c299b
joshneipp <joshneipp@gmail.com>
2015-11-03 09:33:21 -0500

    Change password validations in user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,25 +1,22 @@
 class User < ActiveRecord::Base
-<<<<<<< HEAD
-  has_secure_password
+
+  #has_secure_password
 
   has_many :products
 
   validates :name, presence: true
   # validates :username, presence: true
-  validates :password_digest, presence: true
+  # validates :password_digest, presence: true
 
-=======
+  def self.sign_in_from_omniauth(auth)
+   find_by(provider: auth['provider'], uid: auth['uid']) || create_user_from_omniauth(auth)
+  end
 
-   def self.sign_in_from_omniauth(auth)
-     find_by(provider: auth['provider'], uid: auth['uid']) || create_user_from_omniauth(auth)
-   end
-
-   def self.create_user_from_omniauth(auth)
-    create! do |user|
-      user.provider = auth["provider"]
-      user.uid = auth["uid"]
-      user.name = auth["info"]["name"]
-    end
-   end
->>>>>>> farmerjosh
+  def self.create_user_from_omniauth(auth)
+  create! do |user|
+    user.provider = auth["provider"]
+    user.uid = auth["uid"]
+    user.name = auth["info"]["name"]
+  end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20151102163644) do
-=======
 ActiveRecord::Schema.define(version: 20151103040912) do
->>>>>>> farmerjosh
 
   create_table "images", force: :cascade do |t|
     t.integer  "product_id"
