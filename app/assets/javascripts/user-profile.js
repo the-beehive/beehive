93bed74f411dea09666f56d6c7bab67cf12f6671
Kristine Erickson <ericksonk09@gmail.com>
2015-11-03 20:08:59 -0500

    jQ for user-profile tabs, not working on server yet

---
+++ app/assets/javascripts/user-profile.js
@@ -0,0 +1,11 @@
+$('.profile-tabs > a').on('click', function(){
+  var panelId = (this).attr('href');
+
+  $(this).add(panelId)
+    .addClass('active')
+  .siblings()
+    .removeClass('active');
+
+  }).filter('[href="#orders"]')
+    .trigger('click');
+    
