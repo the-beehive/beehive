7e485c7a27d91f64a75cb498478749f718dcdec7
Ashley Mason <amason@spoonflower.com>
2015-11-04 15:10:17 -0500

    didn't want to stash

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,6 +1,6 @@
 class Image < ActiveRecord::Base
   has_attached_file :uploaded_file
-  # validates_attachment_content_type :uploaded_file, :content_type => /\Aimage\/.*/
+  validates_attachment_content_type :uploaded_file, :content_type => /\Aimage\/.*/
 
 
   belongs_to :product
