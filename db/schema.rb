137f7da44c254eba04989ba74d745cbe10eccf09
joshneipp <joshneipp@gmail.com>
2015-11-04 11:17:44 -0500

    Change user model.

--- db/schema.rb
+++ db/schema.rb
@@ -15,12 +15,8 @@ ActiveRecord::Schema.define(version: 20151104022733) do
 
   create_table "images", force: :cascade do |t|
     t.integer  "product_id"
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
-    t.string   "uploaded_file_file_name"
-    t.string   "uploaded_file_content_type"
-    t.integer  "uploaded_file_file_size"
-    t.datetime "uploaded_file_updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "order_items", force: :cascade do |t|
@@ -66,12 +62,8 @@ ActiveRecord::Schema.define(version: 20151104022733) do
     t.string   "phone"
     t.string   "uid"
     t.string   "provider"
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
-    t.string   "uploaded_file_file_name"
-    t.string   "uploaded_file_content_type"
-    t.integer  "uploaded_file_file_size"
-    t.datetime "uploaded_file_updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
