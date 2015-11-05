01dea1da56497a4fb4829186e844ebd4d3ff84d5
joshneipp <joshneipp@gmail.com>
2015-11-05 09:37:44 -0500

    Change routes to allow logout from users show page.

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -2,19 +2,18 @@ require 'test_helper'
 
 class SessionsControllerTest < ActionController::TestCase
 
-    test '#signup_success' do
-        OmniAuth.config.test_mode = true
-        OmniAuth.config.mock_auth[:facebook] = OmniAuth::AuthHash.new({
-            'provider' => 'facebook',
-            'uid' => '123451234512345',
-            'name' => 'testname'
-        })
-        request.env['omniauth.env'] = OmniAuth.config.mock_auth[:facebook]
+  test '#signup_success' do
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.mock_auth[:facebook] = OmniAuth::AuthHash.new({
+        'provider' => 'facebook',
+        'uid' => '123451234512345',
+        'name' => 'testname'
+    })
+    request.env['omniauth.env'] = OmniAuth.config.mock_auth[:facebook]
 
-        assert_routing "/auth/provider/callback",
-          :controller => "sessions",
-          :action => "create",
-          :provider=>"provider"
-
-    end
+    assert_routing "/auth/provider/callback",
+      :controller => "sessions",
+      :action => "create",
+      :provider => "provider"
+  end
 end
