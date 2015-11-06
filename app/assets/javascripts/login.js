66089614fa95bd00f3020f914639226b0d181d02
Kristine Erickson <ericksonk09@gmail.com>
2015-11-05 22:01:57 -0500

    working test-modal code, need to apply to sign in form now

--- app/assets/javascripts/login.js
+++ app/assets/javascripts/login.js
@@ -1,11 +1,18 @@
-$(document).ready(function(){
-  // LOGIN DROP-DOWN MENU
-  $(".user-login > ul").hide();
-  $('.login-register > .log').on('click', function(){
-    if ($('.user-login > ul').is(':hidden')) {
-      $('.user-login > ul').show();
-    } else {
-      $('.user-login > ul').hide();
-    }
-  });
-});
+// $(function() {
+  function overlay() {
+    modal = document.getElementById("overlay");
+    modal.style.visibility = (modal.style.visibility == "visible") ? "hidden" : "visible";
+  }
+// });
+
+// $(document).ready(function(){
+//   // LOGIN DROP-DOWN MENU
+//   $(".user-login > ul").hide();
+//   $('.login-register > .log').on('click', function(){
+//     if ($('.user-login > ul').is(':hidden')) {
+//       $('.user-login > ul').show();
+//     } else {
+//       $('.user-login > ul').hide();
+//     }
+//   });
+// });
