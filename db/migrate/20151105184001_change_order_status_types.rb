b4a4afc90e8b58f0a3f9f8e412e350b955b04928
joshneipp <joshneipp@gmail.com>
2015-11-08 21:05:21 -0500

    Modify migration for changing order status types.

--- db/migrate/20151105184001_change_order_status_types.rb
+++ db/migrate/20151105184001_change_order_status_types.rb
@@ -1,6 +1,11 @@
 class ChangeOrderStatusTypes < ActiveRecord::Migration
-  def change
+  def self.up
     change_column :orders, :order_status, :integer, default: 1
     change_column :order_items, :order_item_status, :integer, default: 1
+  end
+
+  def self.down
+    change_column :orders, :order_status, :string
+    change_column :order_items, :order_item_status, :string
   end
 end
