fdc99f7e49ba3f5d54e98c70b3c73aea1a18053f
Josh McKelvey <jmckelvey0@gmail.com>
2015-11-10 15:39:54 -0500

    Fix jQuery mixup

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -2,7 +2,7 @@ function highlightOnClick() {
   $(".fabric-design").css("border", "none");
   $(event.target).css("border", "solid 5px red");
   $fabric_id = $(event.target).attr("alt");
-  var _href = $("#add_to_cart").attr("href");
+  var _href = $("#add-to-cart").attr("href");
   var _src = $(event.target).attr("src");
   $("#add-to-cart").attr("href", _href + "&design=" + $fabric_id + "&source=" + _src)
 }
