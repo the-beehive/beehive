667c5afb8ce3b4220dd5a41aa1764a89c1c859eb
Kristine Erickson <ericksonk09@gmail.com>
2015-11-11 21:39:54 -0500

    product thumbnail gallery working

--- app/assets/javascripts/image-slider.js
+++ app/assets/javascripts/image-slider.js
@@ -1,6 +1,11 @@
 $(document).ready(function () {
   var display = $('.col-2-3 img').first().clone();
   $('.img-display').append(display);
-  console.log(display);
+
+  $('.col-2-3 img').on('click', function (){
+    $('.img-display img').remove();
+    var imgSelect = $(this).clone();
+    $('.img-display').append(imgSelect);
+  });
 
 });
