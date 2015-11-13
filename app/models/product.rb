02755bae363774efecb8630c00d40c8e32b6a881
joshneipp <joshneipp@gmail.com>
2015-11-13 18:33:23 -0500

    Users can use partial search terms.

--- app/models/product.rb
+++ app/models/product.rb
@@ -15,7 +15,7 @@ class Product < ActiveRecord::Base
 
   def self.search(search)
     if search
-      where(['name LIKE ?', "#{search}"])
+      where(['name LIKE ?', "%#{search}%"])
     else
       self.all
     end
