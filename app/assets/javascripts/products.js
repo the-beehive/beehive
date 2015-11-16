83f0c9b420247648015e1f9079eba8de7c8199f0
Josh McKelvey <jmckelvey0@gmail.com>
2015-11-16 10:06:29 -0500

    Add a few comments in carts controller

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,3 +1,5 @@
+// On click remove styling from all fabric design images and then highlight the current one.
+// Also alter the add to cart link to represent the design selected.
 function highlightOnClick() {
   $(".fabric-design").css({"border": "none", "outline": "none"});
   $(event.target).css({"border": "solid 0.25em white", "outline": "solid 0.25em #FFAF44"});
@@ -8,6 +10,7 @@ function highlightOnClick() {
   $("#add-to-cart").unbind('click').css({"background": "#FFC577"});
 }
 
+// On page load, deactivate the add to cart link until a fabric design is selected.
 $(function() {
   $(".fabric-design").on("click", highlightOnClick);
   $("#add-to-cart").click(function(e){
